a05fda8d2189f1969d08d1e5fd8493816e07ab6d
hojzas <hojzas@seznam.cz>
2023-02-20 12:51:06 +0100

    Spotify API - get access token

--- visualizer/src/App.js
+++ visualizer/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+
+const CLIENT_ID = "387279f221ed48f68d07577ef0d86ab4";
+const CLIENT_SECRET = "95bb7060f2254649852bfc6fe195b383";
 
 function App() {
+  
+  // Run after render
+  useEffect(() => {
+    // Spotify API - get access token
+    var authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    }
+
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+      .then(result => result.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }

dbbecac11e1fd1d898a9fa37e0c6cc6a0920923a
hojzas <hojzas@seznam.cz>
2023-02-21 12:13:05 +0100

    Display top tracks

--- visualizer/src/App.js
+++ visualizer/src/App.js
@@ -4,7 +4,7 @@ import {useEffect, useState} from 'react';
 
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap'; 
+import { Container, InputGroup, FormControl, Button, Table } from 'react-bootstrap'; 
 
 const CLIENT_ID = "387279f221ed48f68d07577ef0d86ab4";
 const CLIENT_SECRET = "95bb7060f2254649852bfc6fe195b383";
@@ -13,9 +13,10 @@ const CLIENT_SECRET = "95bb7060f2254649852bfc6fe195b383";
 // https://developer.spotify.com/documentation/web-api/reference/#/
 function App() {
   const [artist, setArtist] = useState("");
+  const [artistname, setArtistName] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [topTracks, setTopTracks] = useState([]);
-
+  const [artistGenres, setArtistgenres] = useState([]);
   
   // Run after render
   useEffect(() => {
@@ -47,12 +48,15 @@ function App() {
       }
     }
 
-    // Find artist's ID (top searched result)
+    // Find artist's ID and genres (top searched result)
     var artistID = await fetch('https://api.spotify.com/v1/search?q=' + artist + '&type=artist', authParameters)
       .then(response => response.json())
       .then(data => {
-        return (data.artists.items[0].id)
+        setArtistgenres(data.artists.items[0].genres);
+        setArtistName(data.artists.items[0].name);
+        return (data.artists.items[0].id);
       })
+
 
     // Find artist's top tracks
     var returnedTopTracks = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/top-tracks' + '?market=CZ', authParameters)
@@ -62,11 +66,11 @@ function App() {
       })
   }
 
-  console.log(topTracks);
 
   return (
     <div className="App">
       <h1>Visualizer of Spotify artist's top tracks</h1>
+
       <Container>
         <InputGroup className="mb-3">
           <FormControl
@@ -75,7 +79,7 @@ function App() {
               onKeyPress={event => {
                 if (event.key == "Enter") {
                   findTopTracks();
-                } 
+                }
               }}
               onChange={event => setArtist(event.target.value)}
             />
@@ -83,6 +87,41 @@ function App() {
               Search
             </Button>
         </InputGroup>
+      </Container>
+
+      <Container>
+        <h2>{artistname}</h2>
+        <h5>
+          {artistGenres.map( (genre, idx) => {
+            return (<i>{genre}</i>)
+          })}
+        </h5>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Album</th>
+              <th>Popularity</th>
+              <th>ID</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          {topTracks.map( (topTrack, idx) => {
+            return (
+              <tr>
+                <td>{idx+1}</td>
+                <td>{topTrack.name}</td>
+                <td>{topTrack.album.name}</td>
+                <td>{topTrack.popularity}</td>
+                <td>{topTrack.id}</td>
+              </tr>
+              )
+            })}
+
+          </tbody>
+        </Table>
       </Container>
     </div>
   );

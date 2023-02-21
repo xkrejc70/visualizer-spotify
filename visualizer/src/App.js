33e4523438bb954fc7825ba09f77e7b0fe807698
hojzas <hojzas@seznam.cz>
2023-02-21 09:35:08 +0100

    GET top tracks (hard-coded)

--- visualizer/src/App.js
+++ visualizer/src/App.js
@@ -5,7 +5,10 @@ import {useEffect, useState} from 'react';
 const CLIENT_ID = "387279f221ed48f68d07577ef0d86ab4";
 const CLIENT_SECRET = "95bb7060f2254649852bfc6fe195b383";
 
+
+// https://developer.spotify.com/documentation/web-api/reference/#/
 function App() {
+  const [accessToken, setAccessToken] = useState("");
   
   // Run after render
   useEffect(() => {
@@ -20,8 +23,34 @@ function App() {
 
     fetch('https://accounts.spotify.com/api/token', authParameters)
       .then(result => result.json())
-      .then(data => console.log(data))
+      .then(data => setAccessToken(data.access_token))
+      .catch(err => {
+        console.log(err)
+        // TODO: render error html
+      })
+
+    findTopTracks();
+
   }, [])
+
+  async function findTopTracks() {
+    var authParameters = {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }
+
+    // hard-coded id
+    var artistID = '1dfeR4HaWDbWqFHLkxsg1d';
+    
+    var tracks = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/top-tracks' + '?market=CZ', authParameters)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+  }
 
   return (
     <div className="App">

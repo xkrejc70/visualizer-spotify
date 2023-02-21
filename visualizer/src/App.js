68ef158b8c1f2a43a8fe2a09f5be1ffa563d14eb
hojzas <hojzas@seznam.cz>
2023-02-21 10:15:59 +0100

    Add react-bootstrap

--- visualizer/src/App.js
+++ visualizer/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
 
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap'; 
+
 const CLIENT_ID = "387279f221ed48f68d07577ef0d86ab4";
 const CLIENT_SECRET = "95bb7060f2254649852bfc6fe195b383";
 
@@ -9,6 +13,8 @@ const CLIENT_SECRET = "95bb7060f2254649852bfc6fe195b383";
 // https://developer.spotify.com/documentation/web-api/reference/#/
 function App() {
   const [accessToken, setAccessToken] = useState("");
+  const [topTracks, setTopTracks] = useState([]);
+
   
   // Run after render
   useEffect(() => {
@@ -44,17 +50,37 @@ function App() {
 
     // hard-coded id
     var artistID = '1dfeR4HaWDbWqFHLkxsg1d';
-    
-    var tracks = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/top-tracks' + '?market=CZ', authParameters)
+
+    var returnedTopTracks = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/top-tracks' + '?market=CZ', authParameters)
       .then(response => response.json())
       .then(data => {
         console.log(data);
+        setTopTracks(data.tracks)
       })
   }
 
+  console.log(topTracks);
+
+
   return (
     <div className="App">
-
+      <h1>Visualizer of Spotify artist's top tracks</h1>
+      <Container>
+        <InputGroup className="mb-3">
+          <FormControl
+              placeholder="Search artist"
+              type="input"
+              onKeyPress={event => {
+                if (event.key == "Enter") {
+                  // TODO search artist by name
+                } 
+              }}
+            />
+            <Button>
+              Search
+            </Button>
+        </InputGroup>
+      </Container>
     </div>
   );
 }
